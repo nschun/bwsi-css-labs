73c172e760211ca2efbd227b68e02f351425c26d
nschun <89887919+nschun@users.noreply.github.com>
2026-03-19 21:01:29 -0400

    push action

--- tests/tests_1b.py
+++ tests/tests_1b.py
@@ -37,6 +37,5 @@ def test_invalid_operation():
     with pytest.raises(ValueError, match="Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'."):
         simple_calculator("", 5, 3)                     # Test for empty operation
 
-
 if __name__ == "__main__":
     pytest.main()
